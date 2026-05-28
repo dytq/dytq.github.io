4cab6fed5600790fe2dc74be31f5c427ce73c91c
taariq <taariq@exemple.com>
2026-05-28 16:59:50 +0200

    migration to vue3

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen bg-zinc-950 text-white p-10">
+    <h1 class="text-5xl font-bold text-yellow-400">
+      Portfolio Vue 3 + Tailwind
+    </h1>
+  </div>
+</template>
